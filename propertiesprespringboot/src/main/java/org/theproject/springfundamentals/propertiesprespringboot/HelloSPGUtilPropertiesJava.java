4821c001e32c965ea6acf8f13707b6d6eb0c5af0
lmulcahy <larry.mulcahy@epsilon.com>
2020-05-03 15:26:57 -0600

    Add comments

--- propertiesprespringboot/src/main/java/org/theproject/springfundamentals/propertiesprespringboot/HelloSPGUtilPropertiesJava.java
+++ propertiesprespringboot/src/main/java/org/theproject/springfundamentals/propertiesprespringboot/HelloSPGUtilPropertiesJava.java
@@ -8,6 +8,8 @@ import java.util.Properties;
 
 public class HelloSPGUtilPropertiesJava {
 
+    // This is like using <util:properties> in an XML Spring application context, but instead using Java configuration.
+    // The UtilProperties class uses a PropertiesFactoryBean for the same effect as <util:properties>
     public static void main(String args[]) {
         ApplicationContext context = new AnnotationConfigApplicationContext(UtilProperties.class);
         Properties properties = (Properties) context.getBean("properties");
--- propertiesprespringboot/src/main/java/org/theproject/springfundamentals/propertiesprespringboot/HelloSPGUtilPropertiesXML.java
+++ propertiesprespringboot/src/main/java/org/theproject/springfundamentals/propertiesprespringboot/HelloSPGUtilPropertiesXML.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 
 public class HelloSPGUtilPropertiesXML {
 
+    // Configure properties using a <util:properties/> element in an XML Spring application context
     public static void main(String args[]) {
         ApplicationContext context = new ClassPathXmlApplicationContext("util-properties.xml");
         Properties properties = context.getBean("properties", Properties.class);
