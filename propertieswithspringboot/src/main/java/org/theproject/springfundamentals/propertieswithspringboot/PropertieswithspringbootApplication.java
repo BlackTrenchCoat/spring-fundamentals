938038938185a3a95855dbb015e7d71f0b51a0e5
lmulcahy <larry.mulcahy@epsilon.com>
2020-05-04 06:39:09 -0600

    New propertieswithspringboot project, fix groupids in poms.

---
+++ propertieswithspringboot/src/main/java/org/theproject/springfundamentals/propertieswithspringboot/PropertieswithspringbootApplication.java
@@ -0,0 +1,13 @@
+package org.theproject.springfundamentals.propertieswithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertieswithspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertieswithspringbootApplication.class, args);
+	}
+
+}
---
+++ propertieswithspringboot/src/test/java/org/theproject/springfundamentals/propertieswithspringboot/PropertieswithspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.theproject.springfundamentals.propertieswithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertieswithspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
